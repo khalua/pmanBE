9a18dc0ccf6180dd2390c4679c359311a90783a5
Tony Contreras <633390+khalua@users.noreply.github.com>
2026-02-15 09:30:03 -0800

    prompt!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,8 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
-    "pman-api.yerko.com"
+    "pman-api.yerko.com",
+    "prompt.yerko.com"
   ]
   # Skip DNS rebinding protection for the default health check endpoint.
   config.host_authorization = { exclude: ->(request) { request.path == "/up" || request.path == "/health" } }
