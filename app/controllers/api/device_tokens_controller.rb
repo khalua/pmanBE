02d07960f94df2d459ed54123bceb67e2ee7672d
Tony Contreras <633390+khalua@users.noreply.github.com>
2026-02-16 18:55:50 -0800

    added error handling for notification testing

--- app/controllers/api/device_tokens_controller.rb
+++ app/controllers/api/device_tokens_controller.rb
@@ -1,5 +1,7 @@
 class Api::DeviceTokensController < Api::BaseController
   def create
+    # Remove this token from any other user first (e.g. after switching accounts)
+    DeviceToken.where(token: params[:token]).where.not(user: current_user).destroy_all
     token = current_user.device_tokens.find_or_initialize_by(token: params[:token])
     token.platform = params[:platform] || "ios"
     if token.save
